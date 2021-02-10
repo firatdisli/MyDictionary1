15bc26358a1816707ba14ed1e83f6742571fc1f5
firatdisli <78796132+firatdisli@users.noreply.github.com>
2021-02-11 02:10:34 +0300

    Add files via upload

---
+++ MyDictionary1/MyDictionary1/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary1
+{
+    class MyDictionary<K,V>
+    {
+        K[] Key;
+        V[] Value;
+        public MyDictionary()
+        {
+            Key = new K[0];
+            Value = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] temporaryKey = Key;
+            V[] temporaryValue = Value;
+
+            Key = new K[Key.Length + 1];
+            Value = new V[Value.Length + 1];
+
+            for (int i = 0; i <temporaryKey.Length; i++)
+            {
+                Key[i] = temporaryKey[i];
+                Value[i] = temporaryValue[i];
+
+            }
+            Key[Key.Length - 1] = key;
+            Value[Value.Length - 1] = value;
+        }
+
+        public int Lenght
+        {
+            get { return Key.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary1/MyDictionary1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+
+            Console.WriteLine("Oluşturulan Dictionary de " + myDictionary.Lenght + " elaman vardır");
+            myDictionary.Add("Play", "Oynamak");
+            myDictionary.Add("Read", "Okumak");
+            Console.WriteLine("Oluşturulan Dictionary de " + myDictionary.Lenght + " elaman vardır");
+            myDictionary.Add("Software", "Yazılım");
+            Console.WriteLine("-------Kelimeler----------");
+           
+        }
+    }
+}
